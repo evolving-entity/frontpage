c660543d393eb7182a6b79a6751cff94e295021d
Gibril Darboe <gsdarboe1@gmail.com>
2021-04-17 23:15:56 +0000

    Posts form

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,7 +18,7 @@
                     <a href="{{ route('dashboard') }}" class="p-6">Dashboard</a>
                 </li>
                 <li>
-                    <a href="" class="p-6">Post</a>
+                    <a href="{{ route('posts') }}" class="p-6">Post</a>
                 </li>
             </ul>
             <ul class="flex items-center">
